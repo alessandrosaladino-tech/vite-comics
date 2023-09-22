3c283c961c37cb49bf515ebe0b911dfc453ac3f9
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-22 15:19:50 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+
+
+export default {
+  components: {
+
+  }
+}
+</script>
+
+<template>
+    
+    <header></header>
+
+    <main></main>
+
+    <footer></footer>
+
+
+
+
+</template>
+
+<style>
+
+</style>
