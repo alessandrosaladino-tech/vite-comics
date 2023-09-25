d5b6612a0fa4ba0d38597ea26386144156e3fc5c
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-25 13:06:29 +0200

    Add
    ProductItem to main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,85 +1,95 @@
 <script>
+import products from "../products.js";
+import ProductItem from "./ProductItem.vue"
 export default {
-   name:"AppMain"
+  name: "AppMain",
+  components: {
+    ProductItem,
+  },
+   data() {
+    return {
+      products
+    }
+  }
 
 } 
 </script>
 
 
 <template>
-    
-    <main id="site_main">
+  <main id="site_main">
 
-<div class="main_top">
-  <div class="container">
-    <h2> --> Content goes here &lt;--</h2>
-  </div>
-</div>
+    <div class="main_top">
+      <div class="container">
+        <div class="row row-cols-6 g-4">
 
-<div class="main_store">
-  <div class="container">
-     <div class="row gap-1">
+          <ProductItem :image="product.thumb" :title="product.series" v-for="product in products"></ProductItem>
 
-      <div class="col">
-        <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-        <a href="#">DIGITAL COMICS</a>
+        </div>
       </div>
-      <div class="col">
-        <img src="../assets/img/buy-comics-merchandise.png" alt="">
-        <a href="#">DC MERCHANDISE</a>
-      </div>
-      <div class="col">
-        <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-        <a href="#">SUBSCRIPTION</a>
-      </div>
-      <div class="col">
-        <img src="../assets/img/buy-comics-shop-locator.png" alt="">
-        <a href="#">COMIC SHOP LOCATOR</a>
-      </div>
-      <div class="col">
-        <img src="../assets/img/buy-dc-power-visa.svg" alt="">
-        <a href="#">DC POWER VISA</a>
-      </div>
+    </div>
+
+    <div class="main_store">
+      <div class="container">
+        <div class="row gap-1">
+
+          <div class="col">
+            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+            <a href="#">DIGITAL COMICS</a>
+          </div>
+          <div class="col">
+            <img src="../assets/img/buy-comics-merchandise.png" alt="">
+            <a href="#">DC MERCHANDISE</a>
+          </div>
+          <div class="col">
+            <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+            <a href="#">SUBSCRIPTION</a>
+          </div>
+          <div class="col">
+            <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+            <a href="#">COMIC SHOP LOCATOR</a>
+          </div>
+          <div class="col">
+            <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+            <a href="#">DC POWER VISA</a>
+          </div>
 
 
-     </div>
-  </div>
-</div>
+        </div>
+      </div>
+    </div>
 
-</main>
-
+  </main>
 </template>
 
 
 <style lang="scss" scoped>
-
 @use '../assets/scss/Partials/variables' as *;
+
 .main_top {
-    background-color: $dc_secondary;
-    color: white;
-    height: 150px ;
-    display: flex;
-    align-items: center;
+  background-color: $dc_secondary;
+  color: white;
+  display: flex;
+  align-items: center;
 }
 
 
 .main_store {
-    background-color: $dc_primary;
-    height: 200px;
-    display: flex;
-    align-items: center;
+  background-color: $dc_primary;
+  height: 200px;
+  display: flex;
+  align-items: center;
 
-    & img {
-        width: 50px;
-        height: 60px;
-    }
+  & img {
+    width: 50px;
+    height: 60px;
+  }
 
-    & a {
-        color: $dc_white;
-        text-decoration: none;
-        padding-left: 10px;
-        font-size: 13px;
-    }
+  & a {
+    color: $dc_white;
+    text-decoration: none;
+    padding-left: 10px;
+    font-size: 13px;
+  }
 }
-
 </style>
---
+++ src/components/ProductItem.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+name:"ProductItem",
+props: ["image", "title", "price", "type"]
+}
+</script>
+
+<template>
+    <div class="col pt-5">
+        <div class="card h-50 comics">
+            <img :src="image" alt="" class="card-img-top h-100">
+            <div class="card-bottom">
+                <h5>{{ title }}</h5>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/Partials/variables' as *;
+.comics {
+   background-color: unset;
+   border: none;
+   color: $dc_white;
+}
+
+</style>
